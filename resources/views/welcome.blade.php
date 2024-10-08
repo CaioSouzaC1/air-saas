86b30d405fb059a923b482c7c27d3437dc887835
Matheus Vilela <matheusvilela30@gmail.com>
2024-10-08 01:22:04 -0300

    <CHORE>[Docker] Config docker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <wireui:scripts />
+    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 
 <body>
-    <h1 class="text-red-700 font-bold underline">
+    <h1 class="text-yellow-400 font-bold underline">
         Hello world!
     </h1>
+    <x-alert title="Any Message!" />
+
+    <x-alert title="Any Message!" secondary />
+
+    <x-alert title="Success Message!" positive />
+
+    <x-alert title="Error Message!" negative />
+
+    <x-alert title="Alert Message!" warning />
+
+    <x-alert title="Information Message!" info />
+
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    presets: [require("./vendor/wireui/wireui/tailwind.config.js")],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./vendor/wireui/wireui/src/*.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/WireUi/**/*.php",
+        "./vendor/wireui/wireui/src/Components/**/*.php",
+    ],
     theme: {
         extend: {},
     },
