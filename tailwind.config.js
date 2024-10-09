021b95c22b1e3d6e3519fca67cff65dd1a5f1282
Matheus Vilela <matheusvilela30@gmail.com>
2024-10-08 22:07:48 -0300

    <FEAT>[Mary UI] Remove wireui add MaryUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-
 export default {
-    presets: [require("./vendor/wireui/wireui/tailwind.config.js")],
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./vendor/wireui/wireui/src/*.php",
-        "./vendor/wireui/wireui/ts/**/*.ts",
-        "./vendor/wireui/wireui/src/WireUi/**/*.php",
-        "./vendor/wireui/wireui/src/Components/**/*.php",
+        // You will probably also need these lines
+        "./resources/**/**/*.blade.php",
+        "./resources/**/**/*.js",
+        "./app/View/Components/**/**/*.php",
+        "./app/Livewire/**/**/*.php",
+
+        // Add mary
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
     ],
     theme: {
-        extend: {
-            colors: {
-                primary: colors.sky,
-                secondary: colors.stone,
-                positive: colors.emerald,
-                negative: colors.red,
-                warning: colors.amber,
-                info: colors.blue
-            },
-            fontFamily: {
-                segoe: ['"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'],
-              },
-        },
+        extend: {},
     },
-    plugins: [],
-};
+
+    // Add daisyUI
+    plugins: [require("daisyui")]
+}
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0", // Permite conexões externas
-        port: 5173,
-        hmr: {
-            host: "127.0.0.1", // Força o uso de IPv4
-            protocol: "ws",
-            port: 5173,
-        },
-    },
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
