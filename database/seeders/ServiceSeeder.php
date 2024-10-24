8962a568fe8ac4d7d12c31a901e76d2acb64ea48
Matheus Vilela <matheusvilela30@gmail.com>
2024-10-24 20:07:04 -0300

    <CHORE>[Seeder] create service for worker

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Service;
+use App\Models\Worker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,12 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        Service::factory()->count(10)->create();
+        $workers = Worker::all();
+
+        foreach ($workers as $worker) {
+            Service::factory(10)->create([
+                'worker_id' => $worker->id,
+            ]);
+        }
     }
 }
